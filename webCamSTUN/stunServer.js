92cbfd3f7b544c95ae0fe0f049e8ed3768f067b5
Leo Ardon <leo.ardon.la@gmail.com>
2012-05-11 15:29:11 +0200

    STUN server configured

---
+++ webCamSTUN/stunServer.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2011 Yutaka Takeda <yt0916 at gmail.com>
+ * MIT Licensed
+ */
+
+var stun = require('stun');
+
+// Load config file.
+var fs = require('fs');
+eval(fs.readFileSync('stunServer.conf', encoding="ascii"));
+
+var server = stun.createServer();
+server.setAddress0(settings.STUN_SERVER_ADDR_0);
+server.setAddress1(settings.STUN_SERVER_ADDR_1);
+server.listen();
+
